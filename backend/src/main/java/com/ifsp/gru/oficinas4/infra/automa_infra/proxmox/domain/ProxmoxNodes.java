572a04600475737cd58273615c51ed729535b364
Pedro Clemonini <pclemonini-00@hotmail.com>
2025-10-23 19:59:15 -0300

    node test api - ok

--- backend/src/main/java/com/ifsp/gru/oficinas4/infra/automa_infra/proxmox/domain/ProxmoxNodes.java
+++ backend/src/main/java/com/ifsp/gru/oficinas4/infra/automa_infra/proxmox/domain/ProxmoxNodes.java
@@ -1,10 +1,27 @@
 package com.ifsp.gru.oficinas4.infra.automa_infra.proxmox.domain;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ProxmoxNodes {
-    private String nodes;
-    private String status;
+    private List<Node> data; // <- precisa ser "data"
+    @Data
+    public static class Node {
+        private String node;
+        private String status;
+        private String id;
+        private long maxmem;
+        private long mem;
+        private long maxdisk;
+        private long disk;
+        private double cpu;
+        private int maxcpu;
+        private String ssl_fingerprint;
+        private String type;
+        private String level;
+        private long uptime;
+    }
 
 
 
